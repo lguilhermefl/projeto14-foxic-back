4d8765c4a3cfd31abaa16017f85be7119378ddc8
Alex Souza <alex.webdevelloper@gmail.com>
2022-07-06 20:31:20 -0300

    feat: Cria método para popular banco de dados com produtos
    Cria uma pasta de utilidades para que possamos colocar código reaproveitável. Cria função para adicionar ao banco produtos estáticos. Adiciona mensagem indicando a porta em que o servidor está online.

---
+++ utils/data/products.js
@@ -0,0 +1,74 @@
+export default [
+    {
+        name: 'Oversized Cotton Blouse',
+        description: `Model is 5'9" wearing Size XS TallAnd without further ado, we give you our finest Shopify Theme FOXic! It is a subtle, complex and yet an extremely easy to use template for anyone, who wants to create own website in ANY area of expertise.`,
+        value: 180,
+        category: 'women',
+        images: [
+            'https://img.freepik.com/fotos-gratis/duas-jovens-lindas-sorrindo-hipster-feminina-com-casaco-e-sueter-branco-da-moda_158538-17007.jpg?w=2000'
+        ]
+    },
+    {
+        name: 'Midi Dress with Belt',
+        description: `Model is 5'9" wearing Size XS TallAnd without further ado, we give you our finest Shopify Theme FOXic! It is a subtle, complex and yet an extremely easy to use template for anyone, who wants to create own website in ANY area of expertise.`,
+        value: 250,
+        category: 'women',
+        images: [
+            'https://dondoca.com.br/wp-content/uploads/2021/07/dondoca_com_br-moda-anos-90-pecas-retro-para-montar-looks-modernos-moda-anos-90-inspiracoes.jpg'
+        ]
+    },
+    {
+        name: 'Short Sleeve Blouse',
+        description: `Model is 5'9" wearing Size XS TallAnd without further ado, we give you our finest Shopify Theme FOXic! It is a subtle, complex and yet an extremely easy to use template for anyone, who wants to create own website in ANY area of expertise.`,
+        value: 26,
+        category: 'women',
+        images: [
+            'https://dafitimag.dafiti.com.br/wp-content/uploads/2018/04/Cores-da-esta%C3%A7%C3%A3o-shutterstock_491463319-1024x683.jpg'
+        ]
+    },
+    {
+        name: 'Peg Cropped Cuffed Pants',
+        description: `Model is 5'9" wearing Size XS TallAnd without further ado, we give you our finest Shopify Theme FOXic! It is a subtle, complex and yet an extremely easy to use template for anyone, who wants to create own website in ANY area of expertise.`,
+        value: 74,
+        category: 'women',
+        images: [
+            'https://s2.glbimg.com/66i4-TyC53jPmXQ2FzmarXaaqyo=/0x0:607x426/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_ba3db981e6d14e54bb84be31c923b00c/internal_photos/bs/2021/B/T/5s7bLXQEic23h6MfLeCw/2021-07-08-tendencias-de-inverno-3.jpg'
+        ]
+    },
+    {
+        name: 'Cascade Casual Shirt',
+        description: `Model is 5'9" wearing Size XS TallAnd without further ado, we give you our finest Shopify Theme FOXic! It is a subtle, complex and yet an extremely easy to use template for anyone, who wants to create own website in ANY area of expertise.`,
+        value: 89,
+        category: 'men',
+        images: [
+            'https://i0.wp.com/www.canalmasculino.com.br/wp-content/uploads/2019/05/moda-masculina-destaque-usa-se-quiser.jpg?resize=564%2C705'
+        ]
+    },
+    {
+        name: 'Sport Cotton T-shirt',
+        description: `Model is 5'9" wearing Size XS TallAnd without further ado, we give you our finest Shopify Theme FOXic! It is a subtle, complex and yet an extremely easy to use template for anyone, who wants to create own website in ANY area of expertise.`,
+        value: 67,
+        category: 'men',
+        images: [
+            'https://i.pinimg.com/564x/03/5f/ef/035fef209007b7ab6db305e546c0a2b6.jpg'
+        ]
+    },
+    {
+        name: 'Genuine Leather Strap Watch',
+        description: `Model is 5'9" wearing Size XS TallAnd without further ado, we give you our finest Shopify Theme FOXic! It is a subtle, complex and yet an extremely easy to use template for anyone, who wants to create own website in ANY area of expertise.`,
+        value: 90,
+        category: 'men',
+        images: [
+            'https://i.pinimg.com/736x/d4/aa/52/d4aa522493ff781bb84308676c10e7e5.jpg'
+        ]
+    },
+    {
+        name: 'Fashion Waist Bag',
+        description: `Model is 5'9" wearing Size XS TallAnd without further ado, we give you our finest Shopify Theme FOXic! It is a subtle, complex and yet an extremely easy to use template for anyone, who wants to create own website in ANY area of expertise.`,
+        value: 190,
+        category: 'men',
+        images: [
+            'https://julliterra.com/wp-content/uploads/2021/06/d6daac4a58cea1328ca459db2ddd0f13-523x1024.jpg'
+        ]
+    }
+];
