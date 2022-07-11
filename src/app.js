6303c42e570e75f30d1bf74e65bcd9db734d4b7c
luiz <lguilhermefl@gmail.com>
2022-07-11 15:24:38 -0300

    feat: Envia email de confirmação de compra

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import router from './routes/index.js';
 import populateProductsCollection from './utils/insertProducts.js';
+import dotenv from 'dotenv';
+dotenv.config();
 populateProductsCollection();
 
 const app = express();
--- src/controllers/checkoutController.js
+++ src/controllers/checkoutController.js
@@ -1,10 +1,31 @@
 import db from '../db.js';
+import dotenv from 'dotenv';
+import sgMail from '@sendgrid/mail';
+dotenv.config();
 
 async function saveOrder(req, res) {
     const { user, order } = req.session;
 
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+        to: user.email,
+        from: 'luizguilherme77@gmail.com',
+        subject: 'Pedido Realizado!',
+        text: `Agradecemos sua compra ${user.name}. Seu pedido foi realizado com sucesso!`,
+        html: `<strong>Parabéns pela compra na Foxic ${user.name}. Você receberá seu pedido em breve!</strong>`
+    };
+
     try {
         await db.collection("orders").insertOne({ userId: user._id, ...order });
+        sgMail
+            .send(msg)
+            .then(() => { }, error => {
+                console.error(error);
+
+                if (error.response) {
+                    console.error(error.response.body);
+                };
+            });
         res.sendStatus(201);
     } catch (error) {
         res.status(500).send(error);
